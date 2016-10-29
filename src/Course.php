3697bd2ffaeaf7be9d670c264590aab7bfbe410c
Geese <gisela.chodos@gmail.com>
2016-10-29 14:37:03 -0600

    adding form to insert Course

--- src/Course.php
+++ src/Course.php
@@ -1,12 +1,71 @@
 <?php
 
-/**
- * Created by IntelliJ IDEA.
- * User: Gisela
- * Date: 10/29/2016
- * Time: 1:38 PM
- */
-class Course
-{
+include 'Database.php';
 
+class Course {
+    private $database;
+
+    private $courseId;
+    private $courseCode;
+    private $courseTitle;
+    private $courseCapacity;
+    private $courseCredits;
+    private $deptId;
+
+
+    public function __construct($courseId, $courseCode, $courseTitle, $courseCap, $courseCred, $deptId) {
+        $this->courseId = $courseId;
+        $this->courseCode = $courseCode;
+        $this->courseTitle = $courseTitle;
+        $this->courseCapacity = $courseCap;
+        $this->courseCredits = $courseCred;
+        $this->deptId = $deptId;
+
+        $this->database = new Database();
+    }
+
+    public function getCourseId(){
+        return $this->courseId;
+    }
+
+    public function getCourseCode(){
+        return $this->courseCode;
+    }
+
+    public function getCourseTitle(){
+        return $this->courseTitle;
+    }
+
+    public function getCourseCapacity(){
+        return $this->courseCapacity;
+    }
+
+    public function getCourseCredits(){
+        return $this->courseCredits;
+    }
+
+    public function getDeptId(){
+        return $this->deptId;
+    }
+
+    public function insertNewCourse(){
+
+        $stmtInsert = $this->database->dbh->prepare(
+            "INSERT INTO ZAAMG.Course VALUES (
+              :id, :code, :title, :cap, :cred, :deptId)");
+        # send NULL for course_id because the database auto-increments it
+        $stmtInsert->bindValue("id", NULL);
+        $stmtInsert->bindValue(":code", $this->courseCode);
+        $stmtInsert->bindValue(":title", $this->courseTitle);
+        $stmtInsert->bindValue(":cap", $this->courseCapacity);
+        $stmtInsert->bindValue(":cred", $this->courseCredits);
+        $stmtInsert->bindValue(":deptId", $this->deptId);
+
+        try {
+            $stmtInsert->execute();
+            echo "Success executing Insert";
+        } catch (Exception $e) {
+            echo $e->getMessage();
+        }
+    }
 }
---
+++ src/form_insertCourse.php
@@ -0,0 +1,15 @@
+<!--
+
+This form tests getting Course info and calling
+action_insertClassroom.php with the Submit button
+
+-->
+
+<form action="action_insertCourse.php" method="post">
+ <p>Course Code: <input type="text" name="courseCode" /></p>
+ <p>Course Title: <input type="text" name="courseTitle" /></p>
+ <p>Course Capacity: <input type="text" name="courseCap" /></p>
+ <p>Course Credits: <input type="text" name="courseCred" /></p>
+ <p>Department Id: <input type="text" name="deptId" /></p>
+ <p><input type="submit" /></p>
+</form>
