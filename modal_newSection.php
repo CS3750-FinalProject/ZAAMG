ea0b6fd970343b3c811d33a0411fd637384eb129
absprague <ashtonsprague@mail.weber.edu>
2016-11-09 09:29:27 -0700

    PHP merging

--- modal_newSection.php
+++ modal_newSection.php
@@ -1,20 +1,22 @@
-<?php $database = new Database();?>
+<?php
+require_once "Database.php";
 
-<div class="modal fade" id="newSectionModal" tabindex="-1" role="dialog" aria-labelledby="section-label">
-    <div class="modal-dialog" role="document">
-        <div class="modal-content">
-            <div class="modal-header">
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-                <h4 class="modal-title" id="section-label">Create New Section</h4>
+$database = new Database();
+
+$body = "
+<div class='modal fade' id='newSectionModal' tabindex='-1' role='dialog' aria-labelledby='section-label'>
+    <div class='modal-dialog' role='document'>
+        <div class='modal-content'>
+            <div class='modal-header'>
+                <button type='button' class='close' data-dismiss='modal' aria-label='Close'><span aria-hidden='true'>&times;</span></button>
+                <h4 class='modal-title' id='section-label'>Create New Section</h4>
             </div>
-            <div class="modal-body" style="margin-bottom: 325px; ">
-                <div class="form-group">
-                    <div class="col-xs-7">
-                        <label for="sectionCourse">Course</label>
-                        <select class="form-control" id="sectionCourse"  >
-                            <option value="0">Please Select...</option>
-
-                            <?php
+            <div class='modal-body' style='margin-bottom: 325px; '>
+                <div class='form-group'>
+                    <div class='col-xs-7'>
+                        <label for='sectionCourse'>Course</label>
+                        <select class='form-control' id='sectionCourse'  >
+                            <option value='0'>Please Select...</option>";
 
                             $selectCourse = $database->getdbh()->prepare(
                                 'SELECT course_id, course_code, course_title FROM ZAAMG.Course
@@ -23,21 +25,19 @@
                             $result = $selectCourse->fetchAll(PDO::FETCH_ASSOC);
 
                             foreach($result as $row){
-                                echo "<option value=\"".$row['course_id']
-                                    ."\">".$row['course_code']
-                                    ." ".$row['course_title']
-                                    ."</option>";
+                                echo '<option value=\''.$row['course_id']
+                                    .'\'>'.$row['course_code']
+                                    .' '.$row['course_title']
+                                    .'</option>';
                             }
-                            ?>
-                        </select>
+                        $body .= "</select>
                     </div>
-                    <div class="col-xs-7">
-                        <label for="sectionProfessor">Professor</label>
-                        <select  class="form-control" id="sectionProfessor" >
+                    <div class='col-xs-7'>
+                        <label for='sectionProfessor'>Professor</label>
+                        <select  class='form-control' id='sectionProfessor' >
 
-                            <option value="0">Please Select...</option>
+                            <option value='0'>Please Select...</option>";
 
-                            <?php
                             $selectProf = $database->getdbh()->prepare(
                                 'SELECT prof_id, prof_first, prof_last FROM ZAAMG.Professor
                                   ORDER BY prof_last ASC');
@@ -45,71 +45,72 @@
                             $result = $selectProf->fetchAll();
 
                             foreach($result as $row){
-                                echo "<option value=\"".$row['prof_id']."\">"
-                                    .$row['prof_last'].", ".$row['prof_first']
+                                $body .= "<option value=\"".$row["prof_id"]
+                                    ."\">"
+                                    .$row["prof_last"].", ".$row["prof_first"]
                                     ."</option>";
                             }
-                            ?>
-                        </select>
-                    </div>
-                    <div class="col-xs-7">
-                        <label for="sectionClassroom">Classroom</label>
-                        <select class="form-control" id="sectionClassroom"  >
-                            <option value="0">Please Select...</option>
 
-                            <?php
+                        $body .= "</select>
+                    </div>
+                    <div class='col-xs-7'>
+                        <label for='sectionClassroom'>Classroom</label>
+                        <select class='form-control' id='sectionClassroom'  >
+                            <option value='0'>Please Select...</option>";
+
                             $selectRoom = $database->getdbh()->prepare(
-                                'SELECT classroom_id, campus_name, building_name, classroom_number
+                                "SELECT classroom_id, campus_name, building_name, classroom_number
                                   FROM ZAAMG.Campus c JOIN ZAAMG.Building b
                                   ON c.campus_id = b.campus_id
                                   JOIN ZAAMG.Classroom r
                                   ON b.building_id = r.building_id
-                                  ORDER BY campus_name ASC');
+                                  ORDER BY campus_name ASC");
                             $selectRoom->execute();
                             $result = $selectRoom->fetchAll(PDO::FETCH_ASSOC);
 
                             foreach($result as $row){
-                                echo "<option value=\"".$row['classroom_id']."\">"
-                                    .$row['campus_name'].", "
-                                    .$row['building_name'].": "
-                                    .$row['classroom_number']
+                                $body .= "<option value=\""
+                                .$row["classroom_id"]."\">"
+                                    .$row["campus_name"].", "
+                                    .$row["building_name"].": "
+                                    .$row["classroom_number"]
                                     ."</option>";
                             }
-                            ?>
+        $body .= "
                         </select>
                     </div>
 
                 </div>
-                <div class="col-xs-12">
-                    <hr style="border-width: 2px">
+                <div class='col-xs-12'>
+                    <hr style='border-width: 2px'>
                 </div>
-                <div class="form-group" >
-                    <div class="col-xs-4">
-                        <label for="sectionDays">Days</label>
-                        <select multiple  class="form-control" id="sectionDays"  >
-                        <option value="Monday">Monday</option>
-                            <option value="Tuesday">Tuesday</option>
-                            <option value="Wednesday">Wednesday</option>
-                            <option value="Thursday">Thursday</option>
-                            <option value="Friday">Friday</option>
-                            <option value="Saturday">Saturday</option>
-                            <option value="Sunday">Sunday</option> //are there ever Sunday classes?
+                <div class='form-group' >
+                    <div class='col-xs-4'>
+                        <label for='sectionDays'>Days</label>
+                        <select multiple  class='form-control' id='sectionDays'  >
+                        <option value='Monday'>Monday</option>
+                            <option value='Tuesday'>Tuesday</option>
+                            <option value='Wednesday'>Wednesday</option>
+                            <option value='Thursday'>Thursday</option>
+                            <option value='Friday'>Friday</option>
+                            <option value='Saturday'>Saturday</option>
+                            <option value='Sunday'>Sunday</option> //are there ever Sunday classes?
                         </select>
                     </div>
-                    <div class="col-xs-4">
-                        <label for="sectionStartTime">Start Time</label>
-                        <input type="time" class="form-control" id="sectionStartTime"  >
+                    <div class='col-xs-4'>
+                        <label for='sectionStartTime'>Start Time</label>
+                        <input type='time' class='form-control' id='sectionStartTime'  >
                     </div>
-                    <div class="col-xs-4">
-                        <label for="sectionEndTime">End Time</label>
-                        <input type="time" class="form-control" id="sectionEndTime"  >
+                    <div class='col-xs-4'>
+                        <label for='sectionEndTime'>End Time</label>
+                        <input type='time' class='form-control' id='sectionEndTime'  >
                     </div>
-                    <div class="col-xs-4">
-                        <label for="sectionSemester">Semester</label>
-                        <select class="form-control" id="sectionSemester"  >
-                            <option value="0">Please Select...</option>
+                    <div class='col-xs-4'>
+                        <label for='sectionSemester'>Semester</label>
+                        <select class='form-control' id='sectionSemester'  >
+                            <option value='0'>Please Select...</option>
+";
 
-                            <?php
                             $selectSem = $database->getdbh()->prepare(
                                 'SELECT sem_id, sem_season, sem_year, sem_start_date
                                   FROM ZAAMG.Semester
@@ -118,36 +119,37 @@
                             $result = $selectSem->fetchAll(PDO::FETCH_ASSOC);
 
                             foreach($result as $row){
-                                echo "<option value=\"".$row['sem_id']."\">"
-                                    .$row['sem_year']." "
-                                    .$row['sem_season']
-                                    ."</option>";
+                                $body .= "<option value=\"".$row['sem_id']."\">"
+                                .$row['sem_year']." "
+                                .$row['sem_season']
+                                ."</option>";
                             }
-                            ?>
+                            $body .= "
                         </select>
                     </div>
 
-                    <div class="col-xs-2">
-                        <label for="sectionBlock">Block</label>
-                        <select class="form-control" id="sectionBlock">
-                            <option value="0">Full</option>
-                            <option value="1">First</option>
-                            <option value="2">Second</option>
+                    <div class='col-xs-2'>
+                        <label for='sectionBlock'>Block</label>
+                        <select class='form-control' id='sectionBlock'>
+                            <option value='0'>Full</option>
+                            <option value='1'>First</option>
+                            <option value='2'>Second</option>
                         </select>
                     </div>
 
-                    <div class="col-xs-2">
-                        <label for="sectionCapacity">Capacity</label>
-                        <input type="number" class="form-control" id="sectionCapacity" min="1" >
+                    <div class='col-xs-2'>
+                        <label for='sectionCapacity'>Capacity</label>
+                        <input type='number' class='form-control' id='sectionCapacity' min='1' >
                     </div>
 
                 </div>
             </div>
-            <div class="modal-footer">
-                <span class="error-message"></span>
-                <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-                <button type="button" class="btn btn-primary" id="btn_insertSection">Save</button>
+            <div class='modal-footer'>
+                <span class='error-message'></span>
+                <button type='button' class='btn btn-default' data-dismiss='modal'>Close</button>
+                <button type='button' class='btn btn-primary' id='btn_insertSection'>Save</button>
             </div>
         </div>
     </div>
 </div>
+";
