3cd2306aaa075b0edb653980c82dbc1ec5da1ac5
Geese <gisela.chodos@gmail.com>
2016-11-09 21:18:55 -0700

    More playing with calendar,
    but now I want to revert to an earlier commit
    and start a new branch

--- js/calendar.js
+++ js/calendar.js
@@ -3,24 +3,27 @@ $(document).ready(function() {
     // page is now ready, initialize the calendar...
 
     $('#calendar').fullCalendar({
-        header: {
+        header: false,/*{
             left: 'prev,next today',
             center: 'title',
             right: 'month,agendaWeek,agendaDay,listWeek'
-        },
-        defaultDate: '2016-09-12',
+        }*/
+        //defaultDate: '2016-09-12',
+        allDaySlot: false,
+        defaultView: 'agendaWeek',
+        hiddenDays: [0],
+        columnFormat: 'dddd',
+        firstDay: '1', //Monday
         navLinks: true, // can click day/week names to navigate views
         editable: true,
         eventLimit: true, // allow "more" link when too many events
+        minTime: '06:30:00',
         events: [
-            {
-                title: 'All Day Event',
-                start: '2016-09-01'
-            },
             {
                 title: 'Long Event',
                 start: '2016-09-07',
-                end: '2016-09-10'
+                end: '2016-09-10',
+
             },
             {
                 id: 999,
@@ -32,15 +35,38 @@ $(document).ready(function() {
                 title: 'Repeating Event',
                 start: '2016-09-16T16:00:00'
             },
+
+
             {
-                title: 'Conference',
-                start: '2016-09-11',
-                end: '2016-09-13'
+                title: 'Other Meeting',
+                start: '2016-11-09T10:30:00',
+                end: '2016-11-09T12:30:00',
+                backgroundColor: '#FF33F6'
             },
             {
                 title: 'Meeting',
-                start: '2016-09-12T10:30:00',
-                end: '2016-09-12T12:30:00'
+                start: '2016-11-09T10:30:00',
+                end: '2016-11-09T12:30:00'
+            },
+            {
+                title: 'Other Meeting',
+                start: '2016-11-09T10:30:00',
+                end: '2016-11-09T12:30:00'
+            },
+            {
+                title: 'Meeting',
+                start: '2016-11-09T10:30:00',
+                end: '2016-11-09T12:30:00'
+            },
+            {
+                title: 'Other Meeting',
+                start: '2016-11-09T10:30:00',
+                end: '2016-11-09T12:30:00'
+            },
+            {
+                title: 'Meeting',
+                start: '2016-11-09T10:30:00',
+                end: '2016-11-09T12:30:00'
             },
             {
                 title: 'Lunch',
