ddb7aec35c26d6b337f184b3e5f3bab484810433
Geese <gisela.chodos@gmail.com>
2016-11-09 21:23:30 -0700

    Revert "Playing with fullcalendar.io"
    This reverts commit ef0a6df006a31c674ee1a5b64e98de18196deea8.
    
    # Conflicts:
    #	.idea/workspace.xml
    #	fullcalendar-3.0.1/fullcalendar-3.0.1/demos/agenda-views.html
    #	js/calendar.js

--- index_lollar.php
+++ index_lollar.php
@@ -13,19 +13,11 @@ $body = "
 <!DOCTYPE html>
 <html>
   <head>
-    <link href='css/bootstrap.min.css' rel='stylesheet' />
-    <link href='css/application.css' rel='stylesheet' />
-    <link href='css/fullcalendar.css' rel='stylesheet' />
-
-    <script src='js/jquery.min.js' charset='utf-8'></script>
+    <link href='css/bootstrap.min.css' rel='stylesheet'>
+    <link href='css/application.css' rel='stylesheet'>
     <script src='js/jquery-3.1.1.min.js' charset='utf-8'></script>
     <script src='js/bootstrap.min.js' charset='utf-8'></script>
-    <script src='js/moment.min.js' charset='utf-8'></script>
-    <script src='js/fullcalendar.min.js' charset='utf-8'></script>
-
     <script src='js/processForm.js' charset='utf-8'></script>
-    <script src='js/calendar.js' charset='utf-8'></script>
-
     <title>Project ZAAMG</title>
   </head>
   <body>
@@ -105,14 +97,80 @@ $allSections = $database->getAllSections(null);
 foreach ($allSections as $section){
     $body .= addSection($section);
 }
-$body .= "</table>
-<div id='calendar' ></div>
+$body .= "
+          <!--<tr>
+            <td>CS 1030 <em>Foundations of Computer Science</em></td>
+            <td>Spencer Hilton<br><small><em>spencerhilton@weber.edu</em></small></td>
+            <td><strong>MW: </strong>10:00-11:50<br /><small><em>Full Block</em></small></td>
+            <td><strong>TE 107</strong><br /><small>Ogden Campus</small>
+            <td><img src='img/pencil.png' class='action-edit'/><img src='img/close.png' class='action-delete'></td>
+          </tr>
+          <tr>
+            <td>CS 1400 <em>Fundamentals of Programming</em></td>
+            <td>Spencer Hilton<br><small><em>spencerhilton@weber.edu</em></small></td>
+            <td><strong>TR: </strong>10:00-11:50<br /><small><em>First Block</em></small></td>
+            <td><strong>TE 109E</strong><br /><small>Ogden Campus</small>
+            <td><img src='img/pencil.png' class='action-edit'/><img src='img/close.png' class='action-delete'></td>
+          </tr>
+          <tr>
+            <td>CS 1410 <em>Object-Oriented Programming</em></td>
+            <td>Brian Rague<br><small><em>brianrague@weber.edu</em></small></td>
+            <td><strong>Online</strong><br /><small><em>Second Block</em></small></td>
+            <td><strong>Online</strong><br />&nbsp;</td>
+            <td><img src='img/pencil.png' class='action-edit'/><img src='img/close.png' class='action-delete'></td>
+          </tr>
+          <tr>
+            <td>CS 2130 <em>Computational Structures</em></td>
+            <td>Brian Rague<br><small><em>brianrague@weber.edu</em></small></td>
+            <td><strong>MW: </strong>12:00-1:50<br /><small><em>Full Block</em></small></td>
+            <td><strong>D-207</strong><br /><small>Davis Campus</small></td>
+            <td><img src='img/pencil.png' class='action-edit'/><img src='img/close.png' class='action-delete'></td>
+          </tr>
+          <tr>
+            <td>CS 2350 <em>Web Development</em></td>
+            <td>Garth Tuck<br><small><em>garthtuck@weber.edu</em></small></td>
+            <td><strong>S </strong>10:00-1:50<br /><small><em>First Block</em></small></td>
+            <td><strong>TE 104</strong><br /><small>Ogden Campus</small></td>
+            <td><img src='img/pencil.png' class='action-edit'/><img src='img/close.png' class='action-delete'></td>
+          </tr>
+          <tr>
+            <td>CS 2420 <em>Introduction to Data Structures and Algorithms</em></td>
+            <td>Brad Peterson<br><small><em>bradpeterson@weber.edu</em></small></td>
+            <td><strong>TR: </strong>5:00-6:50<br /><small><em>Second Block</em></small></td>
+            <td><strong>209</strong><br /><small>Davis Campus</small></td>
+            <td><img src='img/pencil.png' class='action-edit'/><img src='img/close.png' class='action-delete'></td>
+          </tr>
+          <tr>
+            <td>CS 2450 <em>Software Engineering I</em></td>
+            <td>Joshua Jensen<br><small><em>joshuajensen@weber.edu</em></small></td>
+            <td><strong>Online</strong><br /><small><em>Full Block</em></small></td>
+            <td><strong>Online</strong><br> &nbsp;</td>
+            <td><img src='img/pencil.png' class='action-edit'/><img src='img/close.png' class='action-delete'></td>
+          </tr>
+          <tr>
+            <td>CS 2550 <em>Database Design and Application Development</em></td>
+            <td>Roberth Hilton<br><small><em>roberthhilton@weber.edu</em></small></td>
+            <td><strong>MW: </strong>8:00-9:50<br /><small><em>Second Block</em></small></td>
+            <td><strong>TE 104</strong><br /><small>Ogden Campus</small></td>
+            <td><img src='img/pencil.png' class='action-edit'/><img src='img/close.png' class='action-delete'></td>
+          </tr>
+          <tr>
+            <td>CS 2705 <em>Network Fundamentals and Design</em></td>
+            <td>Drew Weidman<br><small><em>drewweidman@weber.edu</em></small></td>
+            <td><strong>Online</strong><br /><small><em>First Block</em></small></td>
+            <td><strong>Online</strong><br /><small>&nbsp;</small></td>
+            <td><img src='img/pencil.png' class='action-edit'/><img src='img/close.png' class='action-delete'></td>
+          </tr>
+          <tr>
+            <td>CS 2810 <em>Computer Architecture/Organization</em></td>
+            <td>Joshua Jensen<br><small><em>joshuajensen@weber.edu</em></small></td>
+            <td><strong>TR: </strong>7:00-8:50<br /><small><em>Full Block</em></small></td>
+            <td><strong>TE 102</strong><br /><small>Ogden Campus</small>
+            <td><img src='img/pencil.png' class='action-edit'/><img src='img/close.png' class='action-delete'></td>
+          </tr>-->
       </div>
     </div>
-</div>
-
   </body>
-
 </html>
 ";
 function addSection(Section $section){
