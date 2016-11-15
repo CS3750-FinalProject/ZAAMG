eb166c03efbb746138f8b4f9d82bc1dbad1760f1
absprague <ashtonsprague@mail.weber.edu>
2016-11-15 15:11:08 -0700

    Update to php

--- modal_newCourse.php
+++ modal_newCourse.php
@@ -1,3 +1,5 @@
+<?php
+echo '
 <!-- Modal -->
 <div class="modal fade" id="newCourseModal" tabindex="-1" role="dialog" aria-labelledby="course-label">
     <div class="modal-dialog" role="document">
@@ -6,16 +8,12 @@
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
                 <h4 class="modal-title" id="course-label">Create New Course</h4>
             </div>
-            <div class="modal-body" id="id_form-group" style="margin-bottom: 175px;">
+            <div class="modal-body" id="id_form-group" style="margin-bottom: 150px;">
 
                 <div class="form-group" >
-                    <div class="col-xs-3">
-                        <label for="coursePrefix">Prefix</label>
-                        <input type="text" class="form-control" id="coursePrefix" placeholder="CS" >
-                    </div>
                     <div class="col-xs-4">
-                        <label for="courseNumber">Number</label>
-                        <input type="text" class="form-control" id="courseNumber" placeholder="1010" >
+                        <label for="courseCode">Code</label>
+                        <input type="text" class="form-control" id="courseCode" placeholder="CS 1010" >
                     </div>
                     <div class="col-xs-8">
                         <label for="courseTitle">Title</label>
@@ -24,7 +22,10 @@
 
                 </div>
                 <div class="form-group">
-
+                    <div class="col-xs-2">
+                        <label for="courseCapacity">Capacity</label>
+                        <input type="number" class="form-control" id="courseCapacity" value=30 min=1>
+                    </div>
                     <div class="col-xs-2">
                         <label for="courseCredits">Credits</label>
                         <input type="number" class="form-control" id="courseCredits" value=4 min=1>
@@ -32,9 +33,9 @@
                     <div class="col-xs-8">
                         <label for="courseDepartment">Department</label>
                         <select class="form-control" id="courseDepartment" >
-                            <option value=0 >Please Select...</option>
+                            <option value="0" >Please Select...</option>';
 
-                            <?php
+
                             $database = new Database();
                             $selectDepts = $database->getdbh()->prepare(
                                 'SELECT dept_id, dept_name, dept_code FROM ZAAMG.Department
@@ -42,12 +43,11 @@
                             $selectDepts->execute();
                             $result = $selectDepts->fetchAll();
 
-                            foreach($result as $row){
-                                echo "<option value=".$row['dept_id'].">"
-                                    .$row['dept_name']." (".$row['dept_code'].")"."</option>";
+                            foreach($result as $row) {
+                                echo "<option value=\"" . $row['dept_id'] . "\">"
+                                    . $row['dept_name'] . " (" . $row['dept_code'] . ")" . "</option>";
                             }
-                            ?>
-
+echo '
                         </select>
                     </div>
                 </div>
@@ -60,4 +60,4 @@
             </div>
         </div>
     </div>
-</div>
+</div>';
