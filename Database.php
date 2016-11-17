c3699c92ea6ff88e16a6f1acf0a3e6e55f9454dc
absprague <ashtonsprague@mail.weber.edu>
2016-11-17 13:14:40 -0700

    Added function for campus/building=>classrooms
    Function grabs all classrooms with the associated information of building and campus.

--- Database.php
+++ Database.php
@@ -153,7 +153,27 @@ class Database
         }
     }
 
-
+    public function getClassroomsWithAll($orderBy = null){
+        $allClassrooms = [];
+        $dbh = $this->getdbh();
+        $stmtSelect = $dbh->prepare("SELECT * FROM `W01143557`.`Classroom` JOIN `W01143557`.`Building` ON `building_id`
+            JOIN `W01143557`.`Campus` ON `campus_id`");
+        try{
+            $stmtSelect->execute();
+            $result = $stmtSelect->fetchAll();
+            foreach($result as $index=>$classroomRecord){
+                $allClassrooms[] = new Classroom(  //don't need to put an index number between those brackets, awesome
+                    $classroomRecord['classroom_id'], $classroomRecord['classroom_number'],
+                    $classroomRecord['building_name'], $classroomRecord['campus_name'],
+                    $classroomRecord['classroom_capacity'],
+                    $classroomRecord['classroom_workstations'],
+                    $classroomRecord['building_id']);
+            }
+            return $allClassrooms;
+        }catch(Exception $e){
+            return "getAllClassrooms: ".$e->getMessage();
+        }
+    }
     
     public function getdbh(){
         return $this->dbh;
