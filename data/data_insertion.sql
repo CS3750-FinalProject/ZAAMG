d7ef0861ef654745e8ff11373eec7ec991d820f5
Geese <gisela.chodos@gmail.com>
2016-12-01 10:41:40 -0700

    adding data insertion script/files

---
+++ data/data_insertion.sql
@@ -0,0 +1,14 @@
+#LOAD DATA LOCAL INFILE '\/home\/gc67781\/public_html\/zaamg\/data\/semester_data.txt'
+#INTO TABLE Semester
+#FIELDS TERMINATED BY '|';
+
+#LOAD DATA LOCAL INFILE '\/home\/gc67781\/public_html\/zaamg\/data\/course_data.txt'
+#INTO TABLE Course
+#FIELDS TERMINATED BY '|';
+
+#LOAD DATA LOCAL INFILE '\/home\/gc67781\/public_html\/zaamg\/data\/professor_data.txt'
+#INTO TABLE Professor
+#FIELDS TERMINATED BY '|';
+
+
+#  Uncomment whatever you need as you need it
